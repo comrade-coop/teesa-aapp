8359197c338ea4f89fa78c8dd91d88d511d918a8
Todor Kolev <tkolev@obecto.com>
2025-01-15 23:45:45 +0200

    refactor: improve environment-specific behavior in game state and payment processing logic

--- teesa/app/game/page.tsx
+++ teesa/app/game/page.tsx
@@ -107,7 +107,9 @@ export default function Page() {
     setShowAllMesssages(false);
 
     setPaymentProcessing(true);
-    const paymentResult = await processPayment(walletAddress, wallets);
+    const paymentResult = process.env.NEXT_PUBLIC_ENV_MODE === 'dev' 
+      ? ProcessPaymentResult.Success 
+      : await processPayment(walletAddress, wallets);
     setPaymentProcessing(false);
 
     if (paymentResult != ProcessPaymentResult.Success) {
