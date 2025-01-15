97b54f671bd4f77c7a6f6944d141e8c483e2b929
Alexander Krustev <krustev.alexandr@elveder.com>
2025-01-15 16:26:57 +0000

    Get contract info improvements

---
+++ teesa/app/game/_actions/get-contract-info.tsx
@@ -0,0 +1,22 @@
+'use server';
+
+import { ethers } from "ethers";
+import GameContract from '../../_contracts/Game.json';
+
+export async function getContractInfo() {
+    const gameContractAddress = process.env.GAME_CONTRACT_ADDRESS;
+    const rpcUrl = process.env.RPC_URL;
+
+    const provider = new ethers.JsonRpcProvider(rpcUrl);
+    const contract = new ethers.Contract(gameContractAddress!, GameContract.abi, provider);
+
+    const [prizePoolBN, currentFeeBN] = await Promise.all([
+        contract.prizePool(),
+        contract.currentFee()
+    ]);
+
+    return {
+        prizePool: ethers.formatEther(prizePoolBN),
+        currentFee: ethers.formatEther(currentFeeBN)
+    };
+}
