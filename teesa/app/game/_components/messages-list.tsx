2e35e96e3c8b2eed4b0196d7904c35a43333cb1f
Alexander Krustev <krustev.alexandr@elveder.com>
2025-01-24 12:30:08 +0200

    feat: fix messages auto scroll to bottom on new message send

--- teesa/app/game/_components/messages-list.tsx
+++ teesa/app/game/_components/messages-list.tsx
@@ -2,15 +2,23 @@ import { useEffect, useRef } from "react";
 import { MessageUiStateModel } from "../_models/message-ui-state-model";
 
 export function MessagesList({
-  messages
+  messages,
+  scrollToBottom
 }: {
-  messages: MessageUiStateModel[]
+  messages: MessageUiStateModel[],
+  scrollToBottom: boolean
 }) {
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [messages]);
+    if (!scrollToBottom) {
+      return;
+    }
+
+    setTimeout(() => {
+      messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, 500);
+  }, [scrollToBottom]);
 
   return <div className="flex-1 overflow-auto my-4">
     <div className="mx-4">
