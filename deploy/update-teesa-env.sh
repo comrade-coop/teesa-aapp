c08b54eb7a38a638ecf74dee4d03c47b95f948a4
Alexander Krustev <krustev.alexandr@elveder.com>
2025-01-25 20:40:47 +0200

    feat: Add Eliza configuration to deployment and documentation

--- deploy/update-teesa-env.sh
+++ deploy/update-teesa-env.sh
@@ -2,6 +2,10 @@
 # Update Anthropic API key
 sed -i "s|ANTHROPIC_API_KEY=.*|ANTHROPIC_API_KEY=$DOCKER_ANTHROPIC_API_KEY|g" .env
 
+# Update Eliza configuration
+sed -i "s|ELIZA_API_URL=.*|ELIZA_API_URL=$DOCKER_ELIZA_API_URL|g" .env
+sed -i "s|ELIZA_AGENT_ID=.*|ELIZA_AGENT_ID=$DOCKER_ELIZA_AGENT_ID|g" .env
+
 # Update Privy.io configuration
 sed -i "s|NEXT_PUBLIC_PRIVYIO_APP_ID=.*|NEXT_PUBLIC_PRIVYIO_APP_ID=$DOCKER_PRIVYIO_APP_ID|g" .env
 sed -i "s|PRIVYIO_APP_SECRET=.*|PRIVYIO_APP_SECRET=$DOCKER_PRIVYIO_APP_SECRET|g" .env 
