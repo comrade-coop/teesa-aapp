8f51f36aa953e9716461ce417e0231860900d067
Alexander Krustev <krustev.alexandr@elveder.com>
2025-04-14 01:44:32 +0300

    refactor: replace Ownable with OwnableBasic in TeesaNft contract

--- packages/nft/contracts/teesa-nft.sol
+++ packages/nft/contracts/teesa-nft.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@limitbreak/creator-token-standards/src/access/OwnableInitializable.sol";
+import "@limitbreak/creator-token-standards/src/access/OwnableBasic.sol";
 import "@limitbreak/creator-token-standards/src/erc721c/ERC721C.sol";
 import "@limitbreak/creator-token-standards/src/programmable-royalties/BasicRoyalties.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TeesaNft is ERC721C, BasicRoyalties, Ownable {
+contract TeesaNft is OwnableBasic, ERC721C, BasicRoyalties {
     uint256 private _nextTokenId;
     mapping(uint256 => string) private _tokenURIs;
 
@@ -19,13 +17,14 @@ contract TeesaNft is ERC721C, BasicRoyalties, Ownable {
             teamAddress_,
             1000 // Royalty fee numerator: 1000 = 10%
         )
-        Ownable()
     {}
 
     function mint(
         address to,
         string memory uri
-    ) public onlyOwner returns (uint256) {
+    ) external returns (uint256) {
+        _requireCallerIsContractOwner();
+
         uint256 tokenId = _nextTokenId++;
 
         _mint(to, tokenId);
@@ -34,7 +33,6 @@ contract TeesaNft is ERC721C, BasicRoyalties, Ownable {
         return tokenId;
     }
 
-    /// @inheritdoc IERC721Metadata
     function tokenURI(
         uint256 tokenId
     ) public view virtual override returns (string memory) {
@@ -44,18 +42,9 @@ contract TeesaNft is ERC721C, BasicRoyalties, Ownable {
 
     function supportsInterface(
         bytes4 interfaceId
-    )
-        public
-        view
-        virtual
-        override(ERC721C, ERC2981)
-        returns (bool)
-    {
+    ) public view virtual override(ERC721C, ERC2981) returns (bool) {
         return
             ERC721C.supportsInterface(interfaceId) ||
             ERC2981.supportsInterface(interfaceId);
     }
-
-    // @dev override the _requireCallerIsContractOwner function to allow the owner to mint
-    function _requireCallerIsContractOwner() internal view virtual override {}
 }
