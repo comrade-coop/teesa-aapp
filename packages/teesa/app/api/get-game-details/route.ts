cb09dcd264ea25d09b72616939057ddc3a34a0c8
Alexander Krustev <krustev.alexandr@elveder.com>
2025-04-14 20:39:42 +0300

    feat: enhance game details API

--- packages/teesa/app/api/get-game-details/route.ts
+++ packages/teesa/app/api/get-game-details/route.ts
@@ -1,24 +1,33 @@
 import { getNftContractAddress } from "@teesa-monorepo/nft/src/get-nft-contract-address";
+import { getNetwork } from "@teesa-monorepo/nft/src/networks";
 import { gameState } from "../../_core/game-state";
 
 interface GameDetails {
     gameId: string;
-    winnerAddress: string | undefined;
     nftContractAddress: string;
+    openseaCollectionUrl: string;
+    winnerAddress: string | undefined;
     nftId: string | undefined;
+    openseaNftUrl: string | undefined;
 }
 
 export async function GET(request: Request) {
+    const network = getNetwork();
+
     const gameId = gameState.getId();
-    const winnerAddress = await gameState.getWinnerAddress();
     const nftContractAddress = getNftContractAddress();
+    const openseaCollectionUrl = `${network.openseaUrl}/${nftContractAddress}`;
+    const winnerAddress = await gameState.getWinnerAddress();
     const nftId = await gameState.getNftId();
+    const openseaNftUrl = nftId ? `${openseaCollectionUrl}/${nftId}` : undefined;
 
     const result: GameDetails = {
         gameId: gameId,
-        winnerAddress: winnerAddress,
         nftContractAddress: nftContractAddress,
-        nftId: nftId
+        openseaCollectionUrl: openseaCollectionUrl,
+        winnerAddress: winnerAddress,
+        nftId: nftId,
+        openseaNftUrl: openseaNftUrl
     }
     
     return Response.json(result);
